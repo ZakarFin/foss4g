e946563c319a5e209ec0e302207a4a0b8752090f
ZakarFin <zakarfin@gmail.com>
2016-08-20 13:48:32 +0200

    Feature example added

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ app.use(express.static('public'));
 app.use(express.static('node_modules/oskari-rpc/dist'));
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/map.html');
 });
 
 var server = app.listen(3000, function () {
--- public/js/drawing.js
+++ public/js/drawing.js
@@ -1,28 +1,93 @@
-var drawParams = ['foss4g', 'Polygon', {
-    allowMultipleDrawing : 'single',
-    showMeasureOnMap : true
-}];
-
-var featureCollection;
-
 function startApp(channel) {
     channel.log('Starting the app');
     // start drawing on startup
+    var drawParams = ['foss4g', 'Polygon', {
+        allowMultipleDrawing : 'single',
+        showMeasureOnMap : true
+    }];
     channel.postRequest('DrawTools.StartDrawingRequest', drawParams);
 
+    // handle event for finished drawing
+    var blnFinished = false;
     channel.handleEvent('DrawingEvent', function(data) {
-        if(!data.isFinished) {
+        if(!data.isFinished || blnFinished) {
             return;
         }
+        blnFinished = true;
         console.log('DrawingEvent', data);
-
-        if(!featureCollection) {
-            channel.postRequest('DrawTools.StopDrawingRequest', drawParams);   
-            channel.handleEvent('MapClickedEvent', function(data) {
-                console.log('Map clicked at N: ' + data.lat + ' E:' + data.lon);
-            });
-        }
-        featureCollection = data.geojson;
+        channel.postRequest('DrawTools.StopDrawingRequest', drawParams);
+        // add the user generated feature to map
+        addFeature(channel, data.geojson);
     });
 
+    channel.handleEvent('FeatureEvent', function(e) {
+        console.log('FeatureEvent', e);
+        // when feature is clicked:
+        // add the other and zoom to show both
+        if(e.operation === 'click') {
+            addFeature(channel, geojson, featureStyle);
+        }
+    });
 }
+
+function addFeature(channel, geojsonObject, style) {
+    var params = [geojsonObject, {
+            centerTo: true,
+            cursor: 'pointer',
+            featureStyle : style
+        }];
+
+    channel.postRequest(
+        'MapModulePlugin.AddFeaturesToMapRequest',
+        params
+    );
+}
+// alternative (not-very-good-looking style)
+var featureStyle = {
+    fill: {
+        color: '#ffff00'
+    },
+    stroke : {
+        color: '#ff0000',
+        width: 5
+    }
+};
+// Some other feature as geojson
+var geojson = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [
+            [
+              [
+                373521,
+                6676065
+              ],
+              [
+                373767,
+                6676077
+              ],
+              [
+                373739,
+                6675929
+              ],
+              [
+                373501,
+                6675941
+              ],
+              [
+                373521,
+                6676065
+              ]
+            ]
+          ]
+        },
+        "properties" : {
+            "msg" : "Hello Bonn"
+        }
+      }
+    ]
+  };
