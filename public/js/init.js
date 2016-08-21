b59afd6703086ba75003b384904b7484288ed1f6
ZakarFin <zakarfin@gmail.com>
2016-08-21 18:10:54 +0200

    Ordering bits around to make more sense

--- public/js/init.js
+++ public/js/init.js
@@ -1,12 +1,14 @@
-/** 
+/**
  * Connect to map and call startApp() with
  *  reference to open channel
  */
 var channel = OskariRPC.connect(
-    document.getElementById('map'),
-    'http://www.paikkatietoikkuna.fi'
+  // reference to iframe
+  document.getElementById('map'),
+  // domain that is src on iframe
+  'http://www.paikkatietoikkuna.fi'
 );
 channel.onReady(function() {
-    //channel is now ready and listening.
-    startApp(channel);
-}); 
+  //channel is now ready and listening.
+  startApp(channel);
+});
